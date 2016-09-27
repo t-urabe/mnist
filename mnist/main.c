5aa4798c31331e3f0b602ec1b32386a2c48d57f6
t-urabe <t-urabe@wave.plala.or.jp>
2016-09-27 13:02:34 +0900

    Initial Commit

---
+++ mnist/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mnist
+//
+//  Created by TU on 2016/09/27.
+//  Copyright © 2016年 TU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
